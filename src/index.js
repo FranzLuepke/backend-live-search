363a632ed3ed29e4b5e8b44a4471b2a620c6ca09
FranzLuepke <fk.luepke146@uniandes.edu.co>
2022-07-01 15:37:40 -0500

    couchbase connection

--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 const http = require('http');
+var couchbase = require('couchbase')
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Couchbase authentication
+const clusterConnStr = 'couchbases://cb.<your-endpoint>.cloud.couchbase.com'
+const username = 'Administrator'
+const password = 'Password123!'
+const bucketName = 'travel-sample'
+const scopeName = 'tenant_agent_00'
+const collectionName = 'users'
+
+const cluster = await couchbase.connect(clusterConnStr, {
+  username: username,
+  password: password,
+  timeouts: {
+    kvTimeout: 10000, // milliseconds
+  },
+});
+
+const bucket = cluster.bucket(bucketName);
+const collection = bucket.scope(scopeName).collection(collectionName);
+
+const user = {
+  firstName: 'Will',
+  lastName: 'Smith',
+  email: 'will.smith@test.com',
+  phone: '12345678',
+  consumerId: '10',
+}
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
+  res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en 
-http://${hostname}:${port}/`);
+  console.log(`Server is been executed in http://${hostname}:${port}/`);
 });
+
+async function main() {
+  
+}
+
 
